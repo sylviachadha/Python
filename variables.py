5e9b353eca57131236af592bd71f451750e02a7c
Sylvia Chadha <102805384+sylviachadha@users.noreply.github.com>
2024-03-14 19:40:19 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Variables
+# first_num and second_num r variables here
+# Variables used in adding two numbers
+
+first_num = 1+1+5
+print(first_num)
+second_num = 105+10
+print(second_num)
+
+total = first_num + second_num
+print(total)
