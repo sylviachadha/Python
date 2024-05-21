d76bdb3b441e1bdd21f68a2116bb7a1132712420
Sylvia Chadha <102805384+sylviachadha@users.noreply.github.com>
2024-05-21 21:30:54 +0700

    Add files via upload

---
+++ sy_class_methods.py
@@ -0,0 +1,114 @@
+# Class methods (create behaviours for r coins)
+# ---------------
+
+# Making an object using a class. An object in object-oriented programming has states(attributes) and methods(behaviours).
+#  __init__ and __del__ are constructor and destructor methods in a class.
+
+
+import random
+class Pound:
+
+    def __init__(self, rare=False):
+
+# __init__ Method is the Constructor method.
+# It is called when an object is instantiated and is used to initialize the object's attributes (or state)
+# It takes self (which represents the instance being created) as parameter. Can take more than 1 parameter.
+# (self) is instance being created, if coin1 is created from Pound class, self is coin1.
+# U don't type return inside ur constructor.
+
+        self.rare = rare
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+            
+        self.color = "gold"
+        self.num_edges = 1
+        self.diameter = 22.5   # mm
+        self.thickness = 3.15  # mm
+        self.heads = True
+
+# Methods are defined within the class but not within the constructor.
+# They are part of the class's definition and can use the attributes initialized by the constructor to
+# perform actions or behaviors.
+
+    def rust(self):  # self will be first parameter in class method
+        self.color = "greenish"
+
+    def clean(self):
+        self.color = "gold"
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+
+
+# __del__ Method is the Destructor method.
+# A Destructor is called automatically when r program finishes & is used to destroy an object.
+# It is called when an object is about to be destroyed.
+# It is used to perform any necessary cleanup, such as closing files or releasing resources.
+
+    def __del__(self):
+        print("Coin Spent!")
+        
+        
+
+
+# Creating objects from Pound class
+
+coin1 = Pound(rare = True)  # coin1, object of class 
+coin2 = Pound()                    # coin2, object of class
+
+print(coin1.value)
+print(coin2.value)
+
+coin1.rust()
+# Calling rust method of class pound, just like call append method of class list
+# When you call the rust method on an instance of the Pound class, it modifies the state of the object.
+# The rust method changes the color attribute of the Pound object to "greenish".
+print(coin1.color)
+
+coin1.clean()
+print(coin1.color)
+
+print(coin1.heads)
+coin1.flip()
+print(coin1.heads)
+
+del coin1     # calling destructor
+
+# coin1   this will give error as object coin1 is destroyed already
+
+# Encapsulation
+# --------------------
+
+# Encapsulation in object-oriented programming ensures that:
+
+# 1. Objects maintain their own state independently.
+# 2. Internal states (set of attributes that hold data specific to that object) are hidden from each other and from external access.
+# 3. Access to internal states is controlled through methods, preventing unauthorized or unintended modifications.
+# (External Access refers to the ability to access an object's internal state (attributes) from outside the class itself.
+# Encapsulation restricts this access, meaning that internal states are protected and can only be accessed or modified
+# through the object's methods)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
