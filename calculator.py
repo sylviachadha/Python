dc2cdc68d9292744757be121166468460729bd31
Sylvia Chadha <102805384+sylviachadha@users.noreply.github.com>
2025-05-31 17:19:17 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(n1, n2):
+    """This function will add 2 numbers"""
+    sum1 = n1 + n2
+    return sum1
+
+
+def sub(n1, n2):
+    """This function will subtract 2 numbers"""
+    diff1 = n1 - n2
+    return diff1
+
+
+def mul(n1, n2):
+    """This function will multiply 2 numbers"""
+    return n1 * n2
+
+
+def div(n1, n2):
+    """This function will divide 2 numbers"""
+    return n1 / n2
+
